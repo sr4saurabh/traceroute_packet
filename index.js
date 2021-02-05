2b753c8bda88b5ede47bbe31856baaf19a8bcc67
SAURABH BHARGAV <saurabhbhargav310@gmail.com>
2021-02-05 19:04:58 +0530

    geolocation api working added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const request = require('request');
 var path = require('path');
 var bodyParser = require('body-parser')
 
@@ -32,9 +33,23 @@ app.post('/findway', (req, res) => {
           res.render("output.ejs",{outputpipe:outputpipe});
        
      
-    });
-    
-    
+    });    
+})
+
+app.post('/geo' , (req,res) => {
+  var ip = req.body.ip;
+  var mainurl = 'http://api.ipstack.com/'
+  var middleurl = ''
+  //middleurl has to made of ips seperated by commas
+  var apikey = '?access_key=fce13c65c996f0aa4a5c3193f0a5150b'
+  var requrl = mainurl + middleurl + apikey
+  request('http://api.ipstack.com/106.51.8.242?access_key=fce13c65c996f0aa4a5c3193f0a5150b', { json: true }, (err, res, body) => {
+  if (err) { return console.log(err); }
+  console.log(body.url);
+  console.log(body.explanation);
+  console.log(body);
+});
+
 })
 
 app.listen(port, () => {
