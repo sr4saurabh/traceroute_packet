66c503547edb69ad7706e6c8b871b945c5436c2e
SAURABH BHARGAV <saurabhbhargav310@gmail.com>
2021-02-06 00:07:04 +0530

    now csv of ip created

--- index.js
+++ index.js
@@ -9,6 +9,59 @@ var bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+function giveip(st)
+{
+    console.log(st.length);
+    var outarr = [];
+    var querystring = "";
+    for(var i = 0; i < st.length; i++)
+    {
+      var temp = "";
+      var b = true;
+      var g = 0;
+
+    for(var j = 0; j < st[i].length ; j++)
+    {
+      if(st[i][j]=='*')
+      b = false;
+      if(j>0 && st[i][j]=='s' && st[i][j-1]=='m'){
+      g++;continue;}
+      
+      if(g == 3)
+      temp = temp + st[i][j];
+
+    }
+    if(b == true && g == 3)
+    {
+      temp = temp.trim();
+      outarr.push(temp);
+    }
+
+  }
+      for(var i = 0; i < outarr.length; i++){
+      querystring = querystring + outarr[i] + ',';
+      }
+      querystring = querystring.slice(0,-1);
+      return querystring;
+}
+
+function givelatlong(F)
+{
+  var ip = req.body.ip;
+  var mainurl = 'http://api.ipstack.com/'
+  var middleurl = ''
+  //middleurl has to made of ips seperated by commas
+  var apikey = '?access_key=fce13c65c996f0aa4a5c3193f0a5150b&fields = latitude,longitude'
+  var requrl = mainurl + middleurl + apikey
+
+  request(requrl, { json: true }, (err, res, body) => {
+  if (err)
+      console.log(err);
+  else
+      console.log(body);
+  
+});
+}
 
 app.get('/',(req,res) => {
   res.sendFile(
@@ -27,10 +80,20 @@ app.post('/findway', (req, res) => {
     function (error, stdout, stderr) {
        
        outputpipe = String(stdout);
-       if (error !== null) 
+       if (error !== null){ 
            console.log('exec error: ' + error);
+           res.render("output.ejs",{outputpipe:"cant reach to the server you asked"});
+       }
        else
-          res.render("output.ejs",{outputpipe:outputpipe});
+       {
+        var st = outputpipe.split(/\r?\n/);
+        console.log(st);
+        var iparr = giveip(st);
+        
+       
+        res.render("output.ejs",{outputpipe:iparr});
+       }
+         
        
      
     });    
