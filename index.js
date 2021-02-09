f2f6c87c619184e58eb4a76fa7a06fcb6c118a27
SAURABH BHARGAV <saurabhbhargav310@gmail.com>
2021-02-10 00:04:04 +0530

    v 1.1 ready to deploy

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3000 || process.env.PORT 
 
 const fetch = require("node-fetch");
 const request = require('request');
